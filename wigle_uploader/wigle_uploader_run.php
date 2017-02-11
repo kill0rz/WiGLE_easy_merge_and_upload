8ad3aba3f09cb9541a45c564436a13df6f01868f
kill0rz <kill0rz@users.noreply.github.com>
2017-02-11 15:50:14 +0100

    trying upload; does not work yet

--- wigle_uploader/wigle_uploader_run.php
+++ wigle_uploader/wigle_uploader_run.php
@@ -9,51 +9,6 @@ if (substr($uploades_dir, -1) != "/") {
 	$uploades_dir .= "/";
 }
 
-// This is the entire file that was uploaded to a temp location.
-// $localFile = $_FILES[$fileKey]['tmp_name'];
-
-// function execute($ho, $state, $array, $newsess) {
-// 	global $wigle_api_encoded;
-
-// 	$log_hoster = $array;
-// 	$ch = curl_init();
-
-// 	$url = $log_hoster[$ho][0];
-// 	$postdata = $log_hoster[$ho][1];
-// 	$ref = $log_hoster[$ho][2];
-
-// 	$headers = array(
-// 		'Content-Type:application/json',
-// 		'Authorization: Basic ' . $wigle_api_encoded,
-// 	);
-// 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-// 	curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-// 	curl_setopt($ch, CURLOPT_AUTOREFERER, true);
-// 	curl_setopt($ch, CURLOPT_COOKIESESSION, $newsess);
-// 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-// 	curl_setopt($ch, CURLOPT_HEADER, false);
-// 	curl_setopt($ch, CURLOPT_POST, true);
-// 	curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-// 	curl_setopt($ch, CURLOPT_PROXYAUTH, CURLAUTH_NTLM);
-// 	curl_setopt($ch, CURLOPT_REFERER, $ref);
-// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// 	curl_setopt($ch, CURLOPT_URL, $url);
-// 	curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
-// 	$retu = curl_exec($ch);
-// 	sleep(1);
-// 	return $retu;
-// }
-
-// function getsite($zielurl, $postdata = "", $ref = "", $newsess = true) {
-// 	$log_hoster = array(
-// 		array($zielurl, $postdata, $ref),
-// 	);
-// 	$temp = execute(0, true, $log_hoster, $newsess);
-// 	return $temp;
-// }
-
 // Step 1: Look for any file
 
 if (isset($files_dir) && trim($files_dir) != '') {
@@ -156,5 +111,32 @@ if (isset($files_dir) && trim($files_dir) != '') {
 		}
 
 		// Step 4: Upload to WiGLE
+		class curl {
+			public function __construct($wigle_api_encoded) {
+				$this->wigle_api_encoded = $wigle_api_encoded;
+				$this->ch = curl_init();
+				curl_setopt($this->ch, CURLOPT_POST, 1);
+				curl_setopt($this->ch, CURLOPT_COOKIEJAR, 'cookie.txt');
+				curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, 1);
+				curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($this->ch, CURLOPT_AUTOREFERER, 1);
+			}
+
+			public function upload($file) {
+				curl_setopt($this->ch, CURLOPT_HTTPHEADER, array("Content-type: multipart/form-data", 'Authorization: Basic ' . $this->wigle_api_encoded));
+				curl_setopt($this->ch, CURLOPT_URL, 'https://api.wigle.net/api/v2/file/upload/');
+				$postdata = array(
+					"file[]" => "@/" . realpath($file) . ";type=application/zip",
+					"donate" => "true",
+				);
+				curl_setopt($this->ch, CURLOPT_POSTFIELDS, $postdata);
+				$data = curl_exec($this->ch);
+				echo $data;
+			}
+		}
+
+		$getit = new curl($wigle_api_encoded);
+		$getit->upload($uploades_dir . "upload_to_wigle.zip");
+		@unlink($uploades_dir . "upload_to_wigle.zip");
 	}
 }
